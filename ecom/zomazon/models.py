8b5c4d60a35ad746f828cc475397c371b5144ce7
Balasubramaniam N <balasubramaniam.n83@gmail.com>
2025-02-05 19:41:21 +0530

    no message

--- ecom/zomazon/models.py
+++ ecom/zomazon/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.TextField(20)
+    description = models.TextField(100)
+    price = models.DecimalField(10, 2)
